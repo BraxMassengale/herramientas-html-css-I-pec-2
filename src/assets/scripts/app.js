00b573bbd4fdce019cfe76f4214d831000b96759
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-04-15 11:00:34 +0200

    Final commit

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,2 +1,6 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+const stoicQuote = require('stoic-quotes');
+const quoteBlock = document.querySelector('#quote');
+
+let quote = stoicQuote("marcus");
+quoteBlock.innerText = quote.author + ': ' + quote.quote;
+
